573850d7369395ab54fcc1b317566f41b707eb67
Leo Taylor <leotaylor723@gmail.com>
2018-09-22 10:25:55 -0500

    print customers that are millionaires

---
+++ lingExercises/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lingExercises
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+}
--- lingExercises/Program.cs
+++ lingExercises/Program.cs
@@ -119,16 +119,29 @@ namespace lingExercises
             {
                 Console.WriteLine(number);
             }
+            Console.ReadLine();
 
+            // Build a collection of customers who are millionaires
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
 
-            //foreach (var squared in wheresSquaredo)
-            //{
-            //    var squareList = Math.Sqrt(squared);
-            //    if(squareList % 1 == 0)
-            //    {
-            //        Console.WriteLine(squareList);
-            //    }
-            //}
+            foreach(var customer in customers)
+            {
+                if(customer.Balance >= 1000000)
+                {
+                    Console.WriteLine(customer.Name);
+                }
+            }
             Console.ReadLine();
         }
     }
